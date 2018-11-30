243b051f2f5f65484e9eb680e244c8186c66f252
alex <aa26_ph@yahoo.com>
2018-12-01 01:58:19 +0800

    Note can now displayed

--- src/index.js
+++ src/index.js
@@ -3,32 +3,71 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 const API_HOST = 'http://localhost:3002';
-// const API_HOST = 'https://alex-simple-todo.herokuapp.com/';
+// const API_HOST = 'https://alex-simple-todo.herokuapp.com';
 
-class NoteList extends React.Component {
-    state = {
-        titles: []
-    }
+class TitleList extends React.Component {
+    constructor(props) {
+		super(props);
+		this.state = {titles: [], selectedNoteId: null};
+	}
 
     componentDidMount() {
-        axios.get(`${API_HOST}/titles`)
+        axios.get(`${API_HOST}/titles/alex`)
         .then(res => {
+			// TODO: Add err handling
             this.setState({titles: res.data.titles});
         })
     }
+	
+	handleClick(noteId) {
+		this.setState({selectedNoteId: noteId});
+	}
 
     render() {
         return (
-            <ol>
-                <ul>
-                    {this.state.titles.map(title => <li>{title}</li>)}
-                </ul>
-            </ol>
+			<div>
+				<ul>
+					{this.state.titles.map(title => 
+					<li key={title.noteId}>
+						<a href="#" onClick={() => this.handleClick(title.noteId)}>{title.title}</a>
+					</li>)}
+				</ul>
+				<Note noteId={this.state.selectedNoteId} />
+			</div>
         )
     }
 }
 
+class Note extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {note: {}};
+	}
+	
+	componentDidUpdate(prevProps) {
+		if (this.props.noteId !== prevProps.noteId) {
+			axios.get(`${API_HOST}/note/${this.props.noteId}`)
+			.then(res => {
+				if (res.data.code !== 0) {
+					// TODO: Add err handling
+				}
+				
+				this.setState({note: res.data.note});
+			})
+		}
+	}
+	
+	render() {
+		return (
+			<div>
+				<h4>{this.state.note.title}</h4>
+				<div>{this.state.note.note}</div>
+			</div>
+		)
+	}
+}
+
 ReactDOM.render(
-    <NoteList />,
+    <TitleList />,
     document.getElementById('root')
   );
