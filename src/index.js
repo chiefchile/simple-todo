1f8c28fcf2c80a1f376ca017d5579a9d4c2ef934
alex <aa26_ph@yahoo.com>
2018-12-03 17:22:25 +0800

    added login

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import { default as React } from 'react';
+import { default as React, Component } from 'react';
 import ReactDOM from 'react-dom';
 import ToDo from './todo';
 import './index.css';
+import { Login } from './login';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
+class Main extends Component {
+    state = {
+        user: null
+    };
+
+    login(username, password) {
+        this.setState({ user: username });
+    }
+
+    loginAsGuest() {
+        this.setState({ user: 'guest' });
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.user ?
+                        <ToDo user={this.state.user} /> :
+                        <Login onSubmit={(username, password) => this.login(username, password)} 
+                            onLoginAsGuest={() => this.loginAsGuest()}
+                        />
+                }
+            </div>
+        )
+    }
+}
 
 ReactDOM.render((
-    <ToDo user="alex" />
+    <Main />
 ), document.getElementById('root'));
