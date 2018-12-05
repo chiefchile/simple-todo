2884f2b8042282fe8fc94a7b904e8ce3ed1b91a8
alex <aa26_ph@yahoo.com>
2018-12-05 19:59:40 +0800

    added ts in title-list

---
+++ src/title-list.tsx
@@ -0,0 +1,18 @@
+import { default as React } from 'react';
+import { Title } from './title';
+
+export const TitleList = (props: any) => {
+
+	return (
+		<div>
+			<h4>Notes</h4>
+			<ul>
+				{props.titles.map((title: Title) =>
+					<li key={title._id}>
+						<a href="#" onClick={() => props.onClick(title._id)}>{title.title}</a>
+					</li>)}
+			</ul>
+		</div>
+	)
+
+}
---
+++ src/title.tsx
@@ -0,0 +1,5 @@
+
+export interface Title {
+    _id: string,
+    title: string
+}
