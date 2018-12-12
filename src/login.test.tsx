abfb6cd626125c393f3a1d6bdf01dca2db45a84c
alex <aa26_ph@yahoo.com>
2018-12-12 19:19:28 +0800

    added message.test.tsx

--- src/login.test.tsx
+++ src/login.test.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Login } from './login';
-
 import {render, fireEvent, cleanup, wait} from 'react-testing-library'
 
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup)
 
 it('onSubmit is called when "Login" is clicked', async () => {
-  const mockOnSubmit = jest.fn(() => {});
+  const mockOnSubmit = jest.fn();
   const {getByText, getByTestId, container, asFragment} = render(
     <Login onSubmit={mockOnSubmit} />,
   )
 
   fireEvent.click(getByText('Login'));
   
-  await wait(() => {
-    expect(mockOnSubmit).toHaveBeenCalled();
-  });
+  await wait();
+  expect(mockOnSubmit).toHaveBeenCalled();
 });
 
 it('onLoginAsGuest is called when "Login as Guest" is clicked', async () => {
-  const mockOnLoginAsGuest = jest.fn(() => {});
+  const mockOnLoginAsGuest = jest.fn();
   const {getByText, getByTestId, container, asFragment} = render(
     <Login onLoginAsGuest={mockOnLoginAsGuest} />,
   )
---
+++ src/message.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Message } from './message';
+import {render, fireEvent, cleanup, wait} from 'react-testing-library'
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup)
+
+it('style should be "alert alert-success" if result is successful', () => {
+  const msg = 'Success';
+  const success = {code: 0, msg: msg};
+  const {getByText} = render(
+    <Message result={success} />,
+  )
+  
+  const message = getByText(msg);
+  expect(message.className).toBe('alert alert-success');
+});
+
+it('style should be "alert alert-danger" if result is error', () => {
+  const msg = 'Error';
+  const success = {code: -1, msg: msg};
+  const {getByText} = render(
+    <Message result={success} />,
+  )
+  
+  const message = getByText(msg);
+  expect(message.className).toBe('alert alert-danger');
+});
