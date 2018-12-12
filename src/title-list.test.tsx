9a41c843f832b34a0f4c77761aea96323cdaeb91
alex <aa26_ph@yahoo.com>
2018-12-12 15:46:00 +0800

    finished title-list.test.tsx

--- src/title-list.test.tsx
+++ src/title-list.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { TitleList } from './title-list';
 import renderer from 'react-test-renderer';
 import Title from './title';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-
+Enzyme.configure({adapter: new Adapter()});
 
 it('renders correctly', () => {
   const titles: Title[] = [{title: 'Title2', _id: '1'}];
@@ -11,4 +13,15 @@ it('renders correctly', () => {
     .create(<TitleList titles={titles} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('onClick is called when title is clicked', () => {
+  const titles: Title[] = [{title: 'Title2', _id: '1'}];
+  const onClick = () => {};
+  const mockOnClick = jest.fn(onClick);
+  const titleList = shallow(<TitleList titles={titles} onClick={mockOnClick}  />);
+
+  titleList.find('a').simulate('click');
+  
+  expect(mockOnClick.mock.calls.length).toBe(1);
 });
