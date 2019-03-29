f576d83b8ed5ae716155e5b0170b967d9599342f
alex <aa26_ph@yahoo.com>
2019-03-29 20:03:06 +0800

    changes login css

--- src/login.tsx
+++ src/login.tsx
@@ -1,68 +1,87 @@
-import { default as React } from 'react';
-import { Formik, Form, Field } from 'formik';
-import { Message } from './message';
-import User from './user';
-import Result from './result';
+import { default as React } from "react";
+import { Formik, Form, Field } from "formik";
+import { Message } from "./message";
+import User from "./user";
+import Result from "./result";
 
 interface Props {
-	onSubmit(user: User): void, 
-	onLoginAsGuest(): void, 
-	loginResult: Result | null	
+  onSubmit(user: User): void;
+  onLoginAsGuest(): void;
+  loginResult: Result | null;
 }
 
 export const Login = ({ onSubmit, onLoginAsGuest, loginResult }: Props) => {
+  return (
+    <div className="login col-md-4 col-md-offset-4">
+      <div className="row">
+        <h1 id="logo-text" className="col-md-6 col-md-offset-3">
+          // TODO
+        </h1>
+      </div>
 
-	return (
-		<div className="container">
-			<div className="row">
-				<div id="logo-text" className="col-md-2 col-md-offset-5"><h2>// TODO:</h2></div>
-			</div>
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <Message result={loginResult} />
+        </div>
+      </div>
 
-			<div className="row">
-				<div className="col-md-4 col-md-offset-4">
-					<Message result={loginResult} />
-				</div>
-			</div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(false);
+          onSubmit(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="form-horizontal">
+            <div className="form-group">
+              <div className="col-md-8 col-md-offset-2">
+                <Field
+                  className="form-control"
+                  id="username"
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                />
+              </div>
+            </div>
 
-			<Formik
-				initialValues={{ username: '', password: '' }}
-				onSubmit={(values, { setSubmitting }) => {
-					setSubmitting(false);
-					onSubmit(values);
-				}}
-			>
-				{({ isSubmitting }) => (
-					<Form className="form-horizontal">
-						<div className="form-group">
-							<label className="col-md-1 col-md-offset-4 control-label">Username: </label>
-							<div className="col-md-3">
-								<Field className="form-control" id="username" type="text" name="username" />
-							</div>
-						</div>
+            <div className="form-group">
+              <div className="col-md-8 col-md-offset-2">
+                <Field
+                  className="form-control"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                />
+              </div>
+            </div>
 
-						<div className="form-group">
-							<label className="col-md-1 col-md-offset-4 control-label">Password: </label>
-							<div className="col-md-3">
-								<Field className="form-control" id="password" type="password" name="password" />
-							</div>
-						</div>
-
-						<div className="col-md-1 col-md-offset-4 col-xs-2">
-							<button id="button-login" className="btn btn-primary" type="submit" disabled={isSubmitting}>
-								Login
-				      </button>
-						</div>
-
-						<div className="col-md-3 col-xs-3">
-							<button className="btn btn-primary" onClick={() => onLoginAsGuest()} type="button">
-								Login as Guest
-					    </button>
-						</div>
-
-					</Form>
-				)}
-			</Formik>
-		</div>
-	)
-
-}
+            <div className="form-group">
+              <div className="col-md-3 col-md-offset-2 col-xs-2">
+                <button
+                  id="button-login"
+                  className="btn btn-primary2"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Login
+                </button>
+              </div>
+              <div className="col-md-4 col-xs-3">
+                <button
+                  className="btn btn-primary2"
+                  onClick={() => onLoginAsGuest()}
+                  type="button"
+                >
+                  Login as Guest
+                </button>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
