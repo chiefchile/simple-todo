380d33c1571411b482598807751e4ff917471756
alex <chiefchile@gmail.com>
2019-04-02 00:43:29 +0800

    changed some layout

--- src/note-details.tsx
+++ src/note-details.tsx
@@ -6,14 +6,14 @@ export const NoteDetails = () => {
 		<Fragment>
 		<div className="form-group">
 			<label htmlFor="title" className="col-md-1 control-label">Title: </label>
-			<div className="col-md-9">
+			<div className="col-md-10">
 				<Field className="form-control" id="title" type="text" name="title" />
 			</div>
 		</div>
 
 		<div className="form-group">
 			<label htmlFor="note" className="col-md-1 control-label">Note: </label>
-			<div className="col-md-9">
+			<div className="col-md-10">
 				<Field className="form-control" id="note" component="textarea" name="note" />
 			</div>
 		</div>
