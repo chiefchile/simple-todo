159f8b159de0b09d3373a6ecceebe1b1094c8ba9
alex <aa26_ph@yahoo.com>
2019-04-22 15:34:16 +0800

    added fade out in message

--- src/message.tsx
+++ src/message.tsx
@@ -1,54 +1,58 @@
-import React, { Component } from 'react';
-import Result from './result';
+import React, { Component } from "react";
+import Result from "./result";
 
 interface Props {
-	result: Result | null;
+  result: Result | null;
 }
 
 interface State {
-	style: string
+  style: string;
 }
 
 export class Message extends Component<Props, State> {
-	state = {
-		style: ''
-	}
+  state = {
+    style: ""
+  };
 
-	componentDidMount() {
-		this.setStyle();
-	}
+  componentDidMount() {
+    this.setStyle();
+  }
 
-	componentDidUpdate(prevProps: Props, prevState: State) {
-		if (this.props.result !== prevProps.result) {
-			this.setStyle();
-		}
-	}
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (this.props.result !== prevProps.result) {
+      this.setStyle();
+    }
+  }
 
-	setStyle() {
-		if (this.props.result) {
-			let style = null;
-			if (this.props.result.code === 0) {
-				style = 'alert alert-success';
-			} else {
-				style = 'alert alert-danger';
-			}
-			this.setState({ style: style });
-			this.addFadeOutStyle();
-		}
-	}
+  setStyle() {
+    if (this.props.result) {
+      let style = null;
+      if (this.props.result.code === 0) {
+        style = "alert alert-success";
+      } else {
+        style = "alert alert-danger";
+      }
+      this.setState({ style: style });
+      this.addFadeOutStyle();
+    }
+  }
 
-	addFadeOutStyle() {
-		setTimeout(() => {
-			let style = this.state.style;
-			this.setState({ style: style + ' fade-out' });
-		}, 3000)
-	}
+  addFadeOutStyle() {
+    setTimeout(() => {
+      let style = this.state.style;
+      this.setState({ style: style + " fade-out" });
+    }, 3000);
+  }
 
-	render() {
-		if (this.props.result) {
-			return <div className={this.state.style} role="alert">{this.props.result.msg}</div>
-		} else {
-			return <div className="empty-msg"></div>
-		}
-	}
+  render() {
+    if (this.props.result) {
+      return (
+        <div className={this.state.style} role="alert">
+          {this.props.result.msg}
+        </div>
+      );
+    } else {
+      return <div className="empty-msg" />;
+    }
+  }
 }
