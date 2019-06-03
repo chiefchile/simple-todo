6b46f051857346e4ea5d84b27a1539acb7097c97
alex <aa26_ph@yahoo.com>
2019-06-03 14:21:03 +0800

    Add app component

---
+++ src/app.tsx
@@ -0,0 +1,58 @@
+import { default as React, Component } from "react";
+import { default as Todo, API_HOST } from "./todo";
+import "./index.css";
+import { Login } from "./login";
+import axios from "axios";
+import User from "./user";
+import IResult, { Result } from "./result";
+
+interface State {
+  loginResult: IResult | null;
+  authToken?: string;
+}
+
+export class App extends Component<any, State> {
+  state = {
+    loginResult: null,
+    authToken: undefined
+  };
+
+  login(user: User): void {
+    axios
+      .post(`${API_HOST}/api-token-auth/`, user)
+      .then(res => {
+        console.log(res);
+        this.setState({ authToken: res.data.token });
+      })
+      .catch(error => {
+        this.setState({
+          loginResult: new Result(
+            error.response.status,
+            "Invalid username/password pair"
+          )
+        });
+        console.log(error);
+      });
+  }
+
+  loginAsGuest(): void {
+    let guest: User = { username: "guest", password: "guest" };
+    this.login(guest);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.authToken ? (
+          <Todo authToken={this.state.authToken} />
+        ) : (
+          <Login
+            onSubmit={(user: User) => this.login(user)}
+            onLoginAsGuest={() => this.loginAsGuest()}
+            loginResult={this.state.loginResult}
+          />
+        )}
+      </div>
+    );
+  }
+}
