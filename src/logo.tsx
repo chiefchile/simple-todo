cd5ed388a3b5bf6a25f9484c5f7bf1ebef2456fd
Alex <aa26_ph@yahoo.com>
2020-08-14 20:23:58 +0800

    change logo color

--- src/logo.tsx
+++ src/logo.tsx
@@ -7,7 +7,7 @@ export const Logo = (props: Props) => {
   let className = props.isCenter ? "logo" : "logo logo-main";
   return (
     <div className={className}>
-      <span id="logo-slash">/</span>/ TODO
+      <span id="logo-slash">// </span>TODO
     </div>
   );
 };
